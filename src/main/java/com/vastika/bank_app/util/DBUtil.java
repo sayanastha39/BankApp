4cb7370840bd215f83366b82b9e6bdf549bbfb27
unknown <sikar.samon@gmail.com>
2020-08-10 16:52:32 -0500

    removed psswd

--- src/main/java/com/vastika/bank_app/util/DBUtil.java
+++ src/main/java/com/vastika/bank_app/util/DBUtil.java
@@ -6,7 +6,7 @@ public class DBUtil {
 	public static final String DRIVER_NAME ="com.mysql.jdbc.Driver";
 	public static final String URL = "jdbc:mysql://localhost:3306/jdbc_demo_db";
 	public static final String USER_NAME ="root";
-	public static final String PASSWORD ="9751";
+	public static final String PASSWORD ="";
 	
 	public static Connection getConnection() throws SQLException, ClassNotFoundException {
 		Class.forName(DRIVER_NAME);
