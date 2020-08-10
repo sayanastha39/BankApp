73c6af73dd0b950c6af1c1960412345fbef59e01
Nabin Neupane <coolnabinn@nabins-mbp.attlocal.net>
2020-08-10 16:50:36 -0400

    Fixing driver not found error

---
+++ src/main/java/com/vastika/bank_app/Util/DBUtil.java
@@ -0,0 +1,26 @@
+package com.vastika.bank_app.Util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * @author : Nabin Neupane
+ *
+ *
+ * @date:	 Aug 10, 2020 
+ **/
+public class DBUtil {
+
+	
+	public static final String URL= "jdbc:mysql://localhost:3306?serverTimezone=UTC";
+	public static final String USER_NAME ="root";
+	public static final String PASSWORD ="vastika1";
+	
+	public static Connection getConnection() throws ClassNotFoundException, SQLException{
+		
+		
+		return DriverManager.getConnection(URL,USER_NAME, PASSWORD);
+	}
+
+}
